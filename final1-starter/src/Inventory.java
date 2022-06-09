878d5cf249a3ddca6329424c1912f8574b1f4836
soph-song <87740790+soph-song@users.noreply.github.com>
2022-06-08 19:10:58 -0700

    update

--- final1-starter/src/Inventory.java
+++ final1-starter/src/Inventory.java
@@ -4,3 +4,40 @@ public interface Inventory {
 	int trackOrder(String id);
 	boolean fulfillNextOrder();
 }
+
+interface Computer {
+    // Returns the model name of the computer as a string
+    String getModel();
+}
+
+class Order {
+	String id;
+	int status;
+	Computer model;
+
+	public Order(String id, int status, Computer model) {
+		this.id = id;
+		this.status = status;
+		this.model = model;
+	}
+
+    // Returns the Id of this order
+    String getId() {
+		return id;
+	}
+
+    // Returns the status code of this order
+    int getStatus() {
+		return status;
+	}
+
+    // Sets the status code of this order
+    void setStatus(int status) {
+		this.status = status;
+	}
+
+    // Returns the Computer of this order
+    Computer getProduct() {
+		return model;
+	}
+}
--- final1-starter/src/Warehouse.java
+++ final1-starter/src/Warehouse.java
@@ -1,4 +1,87 @@
+import java.util.ArrayList;
 
 public class Warehouse implements Inventory{
+    int[] stock;
+    Computer[] models;
+    ArrayList<Order> orders;
+    public void Warehouse(int[] stock, Computer[] models) {
+        this.stock = stock;
+        this.models = models;
+        orders = new ArrayList<>();
+    }
+    @Override
+    public boolean makeNewOrder(Order o) {
+        if (o == null) {
+        return false;
+        }
+
+        for (Order ord: orders) {
+            if (ord.getId().equals(o.getId())) {
+                return false;
+            }
+        }
+        int index = getIndex(o);
+        // if we didn't find requested model or Out of stock
+        if (!available(index)) {
+            o.setStatus(0);
+            orders.add(o);
+            return false;
+        }
+        else {
+            o.setStatus(1);
+            orders.add(o);
+            return true;
+        }
+    }
+    @Override
+	public int trackOrder(String id) {
+        if (id == null) {
+            return -1;
+        }
+        for (Order o: orders) {
+            if (o.getId().equals(id)) {
+                return o.getStatus();
+            }
+        }
+        return -1;
+    }
+
+    @Override
+	public boolean fulfillNextOrder() {
+        for (Order o: orders) {
+            //found next idle order
+            if (o.getStatus() == 1) {
+                int index = getIndex(o);
+                if (available(index)) {
+                    o.setStatus(3);
+                    stock[index] -=1;
+                    return true;
+                }
+                else {
+                    o.setStatus(2);
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    private int getIndex(Order o) {
+        int index = 0;
+        for (int i = 0; i < models.length; ++i) {
+            if (models[i].equals(o.getProduct())) {
+                break;
+            }
+            ++index;
+        }
+        return index;
+    }
+
+    private boolean available(int index) {
+        if (index >= models.length || stock[index] <= 0) {
+            return false;
+        }
+        return true;
+    }
 
 }
